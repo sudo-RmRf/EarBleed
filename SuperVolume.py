982ef8b24fa130cd91f53f7edc532e834bbc1408
sudo-RmRf <stevenhollar2009@gmail.com>
2017-04-19 08:21:29 -0500

    Add files via upload

--- SuperVolume.py
+++ SuperVolume.py
@@ -3,25 +3,23 @@ from Tkinter import *
 import subprocess
 import os
 
-master = Tk()
-master.title("Super Volume")
+master = Tk(className="supervolume")
+master.wm_title("Super Volume")
 master.geometry("200x75+100+50")
 
 subprocess.call('pacmd list-sinks | grep "index" > /home/tyler/SuperVolume/currentSink.txt', shell=True)
-subprocess.call("sed 's/[ * index: ]//g' /home/tyler/SuperVolume/currentSink2.txt", shell=True)
+subprocess.call("sed 's/[ * index: ]//g' /home/tyler/SuperVolume/currentSink.txt > /home/tyler/SuperVolume/currentSink2.txt", shell=True)
 z=open("/home/tyler/SuperVolume/currentSink2.txt", "r")
 sink = z.read()
+sink = sink.strip()
 
 def set_vol(val):
-    #subprocess.call("pactl -- set-sink-volume "+sink+" "+value, shell=True)
-    value = val+"%"
-    #command = "pactl -- set-sink-volume "+sink+" "+value
-    #subprocess.call(command)
-    #subprocess.call("echo '" +val+ "' > /home/tyler/SuperVolume/SVlog.txt", shell=True)
-    command_args = ["pactl", "--", "set-sink-volume", sink, value]
-    subprocess.call(command_args)
-    echo val > "/home/tyler/SuperVolume/SVlog.txt"
-    
+    subprocess.call("pactl -- set-sink-volume "+sink+" "+val+"%", shell=True)
+    #com = sink+" "+value
+    #command = "pactl -- set-sink-volume "+value+sink+""
+    #print com
+    subprocess.call("echo '" +val+ "' > /home/tyler/SuperVolume/SVlog.txt", shell=True)
+
 w = Scale(master, length=175, from_=0, to=300, orient=HORIZONTAL, command=set_vol)
 w.pack()
 
@@ -31,12 +29,12 @@ def last_vol():
     w.set(lastVol)
 
 if os.path.exists ('/home/tyler/SuperVolume'):
-    subprocess.call("sed 's/[ % ]//g' /home/tyler/SuperVolume/SVlog.txt", shell=True)
+    #subprocess.call("sed 's/[ % ]//g' /home/tyler/SuperVolume/SVlog.txt", shell=True)
     last_vol()
 else:
     subprocess.call("mkdir /home/tyler/SuperVolume", shell=True)
     subprocess.call("echo '0' > /home/tyler/SuperVolume/SVlog.txt", shell=True)
-    subprocess.call("sed 's/[ % ]//g' /home/tyler/SuperVolume/SVlog.txt", shell=True)
+    #subprocess.call("sed 's/[ % ]//g' /home/tyler/SuperVolume/SVlog.txt", shell=True)
     last_vol()
     
 mainloop()
