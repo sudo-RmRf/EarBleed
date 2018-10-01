1cad0a9878337fce4e385beda45402161de0e127
sudo-RmRf <stevenhollar2009@gmail.com>
2018-10-01 01:58:27 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ then
   echo "sudo rm -f /bin/supervolume" > ${PWD}/uninstall.sh
   echo "sudo rm -f /usr/share/applications/SuperVolume.desktop" >> ${PWD}/uninstall.sh
   echo "sudo rm -rf ../SuperVolume" >> ${PWD}/uninstall.sh
-  sudo chnod +x ./uninstall.sh 
+  sudo chmod +x ./uninstall.sh 
   echo "Icon=""${PWD}""/icon.ico" >> ${PWD}/SuperVolume.desktop
   sudo chmod +x "${PWD}"/SuperVolume.py
   sudo chmod +x "${PWD}"/svstart.sh
