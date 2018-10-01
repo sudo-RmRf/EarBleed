11b761cdc75ce003ee427c06adf4bc50a94b5ae4
sudo-RmRf <stevenhollar2009@gmail.com>
2018-10-01 01:10:06 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
 sudo apt install python3 -y
 sudo apt install python3-tk -y
-sudo mkdir /usr/share/SuperVolume
-sudo cp ./*.* /usr/share/SuperVolume/
-sudo mv /usr/share/SuperVolume/SuperVolume.desktop /usr/share/applications/SuperVolume.desktop
-sudo chmod +x /usr/share/SuperVolume/Supervolume.py
-sudo chmod +x /usr/share/svstart.sh
-sudo ln -s /usr/share/SuperVolume/svstart.sh /bin/supervolume
+echo "sudo rm -f /bin/supervolume" > ${PWD}/uninstall.sh
+echo "sudo rm -f /usr/share/applications/SuperVolume.desktop" >> ${PWD}/uninstall.sh
+echo "sudo rm -rf ../SuperVolume" >> ${PWD}/uninstall.sh
+sudo chnod +x ./uninstall.sh 
+echo "Icon=""${PWD}""/icon.ico" >> ${PWD}/SuperVolume.desktop
+sudo chmod +x "${PWD}"/SuperVolume.py
+sudo chmod +x "${PWD}"/svstart.sh
+sudo ln -s "${PWD}"/svstart.sh /bin/supervolume
+sudo cp "${PWD}"/SuperVolume.desktop /usr/share/applications/SuperVolume.desktop
 
 
